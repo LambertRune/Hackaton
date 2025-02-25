54b1b1ae9415357c5507c71ddd5c7bb7c18f042d
lambertR <rune.lambert@student.vives.be>
2025-02-25 16:03:31 +0100

    d

--- frontend/src/app/service/bicycleparkingService.ts
+++ frontend/src/app/service/bicycleparkingService.ts
@@ -7,11 +7,21 @@ import axios from 'axios';
   providedIn: 'root'
 })
 export class BicycleParkingService {
-  private apiUrl = 'http://localhost:3000/api/details/all'; // Adjust port as needed
+  private apiUrl = 'http://localhost:3000/api'; // Adjust port as needed
 
   async getDetails(){
     try{
-      const response = await axios.get(this.apiUrl);
+      const response = await axios.get(this.apiUrl+'/details/all');
+      return response.data;
+    }
+    catch(error){
+      console.error('Error'); 
+      return [];
+    }
+  }
+  async getDetailsFiltered(){
+    try{
+      const response = await axios.get(this.apiUrl+'/filter/all/?isCovered=true');
       return response.data;
     }
     catch(error){
